b3b8644e110ace1b0372a755300b3ef4889834ca
ttnppedr <ttnppedr@fastmail.com>
2023-07-31 10:38:31 +0800

    add route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/cache', function () {
+    return Carbon::now()->timezone('asia/taipei')->format('Y-m-d H:i:s');
+});
+
+Route::get('/no-cache', function () {
+    return Carbon::now()->timezone('asia/taipei')->format('Y-m-d H:i:s');
 });
