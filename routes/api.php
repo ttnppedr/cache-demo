e2fcf4c280f9ee030bfda9069259643a2c952663
ttnppedr <ttnppedr@fastmail.com>
2023-08-09 10:27:14 +0800

    change method to any

--- routes/api.php
+++ routes/api.php
@@ -26,13 +26,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/cache', function () {
+    Route::any('/cache', function () {
         return response()
             ->json(Carbon::now()->timezone('asia/taipei')->format('Y-m-d H:i:s'))
             ->setCache(['max_age' => 60, 'public' => true]);
     });
 
-    Route::get('/no-cache', function () {
+    Route::any('/no-cache', function () {
         return response()->json(Carbon::now()->timezone('asia/taipei')->format('Y-m-d H:i:s'));
     });
 });
